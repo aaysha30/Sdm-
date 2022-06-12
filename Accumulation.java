1abb7ab5c025a5004d0fc27d9e4514489f24c4db
aaysha <chouhanaaysha@gmail.com>
2022-06-12 11:55:29 +0530

    Added

--- Accumulation.java
+++ Accumulation.java
@@ -2,7 +2,7 @@ class Accumulation{
 public static void main(String []args){
 int i,term=1;
 for(i=1; i<=5; i++){
-
+System.out.println("------------------");
 System.out.print(" "+term);
 term=term+i;
 }
